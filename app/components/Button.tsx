40451790b91fbf951c8cd7563b44a08a118a58db
devambarbhaya <22b2113@iitb.ac.in>
2024-01-28 16:59:31 +0530

    modal component and settings functionality

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -10,6 +10,7 @@ interface ButtonProps {
   secondary?: boolean;
   danger?: boolean;
   disabled?: boolean;
+  primary?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -20,6 +21,7 @@ const Button: React.FC<ButtonProps> = ({
   secondary,
   danger,
   disabled,
+  primary,
 }) => {
   return (
     <button
@@ -32,7 +34,7 @@ const Button: React.FC<ButtonProps> = ({
         fullWidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
         danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
-        !secondary && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+        primary && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
       )}
     >
       {children}
